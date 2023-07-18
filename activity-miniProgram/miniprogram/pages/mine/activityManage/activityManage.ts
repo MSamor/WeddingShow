02279681574854518b0790efeba26814a0b9fce9
MaoSi <1601466212@qq.com>
2023-07-18 15:24:41 +0800

    Feat: win user info

--- activity-miniProgram/miniprogram/pages/mine/activityManage/activityManage.ts
+++ activity-miniProgram/miniprogram/pages/mine/activityManage/activityManage.ts
@@ -2,82 +2,92 @@ import api from "../../../utils/manage/activityApiMange"
 let app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        list: []
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text: "",
+    list: [],
+    newList: []
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
-        api.getActivityPrizes({ code: "timePrize" }).then((res) => {
-            console.log(res);
-            if (res.code == 200) {
-                this.setData({
-                    list: res.data
-                })
-            }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    api.getActivityPrizes({ code: "timePrize" }).then((res) => {
+      console.log(res);
+      if (res.code == 200) {
+        this.setData({
+          list: res.data,
+          newList: res.data
         })
-    },
+      }
+    })
+  },
 
-    onChange(e: any) {
-        console.log(e);
-        
-        api.setGetInfo({
-            openid: app.globalData.openid,
-            state: e.detail.value
-        })
-    },
+  textChange(e: any) {
+    let newList = this.data.list.filter((item: any) => {
+      return (item.openid.indexOf(e.detail.value) >= 0)
+    })
+    this.setData({
+      newList: newList
+    })
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  onChange(e: any) {
+    api.setGetInfo({
+      openid: app.globalData.openid,
+      state: e.detail.value
+    })
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+  },
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+  },
 
-    }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
